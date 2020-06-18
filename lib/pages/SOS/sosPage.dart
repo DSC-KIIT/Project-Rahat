8cb717bbda1c5573ba3581511e3767579a52bf6a
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-06-18 17:54:38 +0530

    sos ui done

--- lib/pages/SOS/sosPage.dart
+++ lib/pages/SOS/sosPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rahat/pages/profile/screens/addPeople.dart';
 
 class SOSPage extends StatefulWidget {
   @override
@@ -22,72 +23,72 @@ class _SOSPageState extends State<SOSPage> {
         centerTitle: true,
       ),
       body: Container(
-          child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 30.0),
-            child: Center(
-                child: Text('CHOOSE ONE',
-                    style: TextStyle(color: Colors.black, fontSize: 25))),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Center(
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: GestureDetector(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.black, width: 1),
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: Column(children: <Widget>[
-                            Container(
-                                height: 110,
-                                width: 110,
-                                child: Image.asset(
-                                    'assets/images/sos/earthquake.jpg')),
-                            Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text('EARTHQUAKE'),
-                            )
-                          ]),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: GestureDetector(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.black, width: 1),
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: Column(children: <Widget>[
-                            Container(
-                                height: 110,
-                                width: 100,
-                                child: Image.asset(
-                                    'assets/images/sos/tsunami.png')),
-                            Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text('TSUNAMI'),
-                            )
-                          ]),
-                        ),
-                      ),
-                    )
-                  ]),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                  child: Text('CHOOSE ONE',
+                      style: TextStyle(color: Colors.black, fontSize: 25))),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Center(
-              child: Row(
+            SizedBox(height: 60.0),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.black, width: 1),
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Column(children: <Widget>[
+                              Container(
+                                  height: 110,
+                                  width: 110,
+                                  child: Image.asset(
+                                      'assets/images/sos/earthquake.jpg')),
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text('EARTHQUAKE'),
+                              )
+                            ]),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.black, width: 1),
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Column(children: <Widget>[
+                              Container(
+                                  height: 110,
+                                  width: 100,
+                                  child: Image.asset(
+                                      'assets/images/sos/tsunami.png')),
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text('TSUNAMI'),
+                              )
+                            ]),
+                          ),
+                        ),
+                      )
+                    ]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Padding(
@@ -120,25 +121,58 @@ class _SOSPageState extends State<SOSPage> {
                             side: BorderSide(color: Colors.black, width: 1),
                             borderRadius: BorderRadius.circular(6),
                           ),
-                          child: Column(children: <Widget>[
-                            Container(
-                                height: 110,
-                                width: 110,
-                                child:
-                                    Image.asset('assets/images/sos/flood.png')),
-                            Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text('FLOOD'),
-                            )
-                          ]),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  height: 110,
+                                  width: 110,
+                                  child: Image.asset(
+                                      'assets/images/sos/flood.png')),
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text('FLOOD'),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     )
-                  ]),
+                  ],
+                ),
+              ),
             ),
-          )
-        ],
-      )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(35.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => AddPeople()));
+                      {}
+                    },
+                    child: Container(
+                      color: Colors.orange,
+                      height: 45.0,
+                      width: 320.0,
+                      child: Center(
+                        child: Text(
+                          'NEXT',
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
