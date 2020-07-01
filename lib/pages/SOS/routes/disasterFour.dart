de297e519d5a87026e88c8da34102011c3880201
sayannath <sayannath235@gmail.com>
2020-07-01 20:06:32 +0530

    Push of the day

--- lib/pages/SOS/routes/disasterFour.dart
+++ lib/pages/SOS/routes/disasterFour.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class DisasterFour extends StatefulWidget {
@@ -33,6 +34,49 @@ class _DisasterFourState extends State<DisasterFour> {
                   style: TextStyle(color: Colors.black, fontSize: 25))),
         ),
         SizedBox(height: 20),
+        StreamBuilder(
+          stream: Firestore.instance
+              .collection("users")
+              .document(widget.uid)
+              .collection("person")
+              .snapshots(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot details = snapshot.data.documents[index];
+                  return details == null
+                      ? Center(
+                          child: Text("Loading..."),
+                        )
+                      : GestureDetector(
+                          child: Card(
+                            elevation: 3.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListTile(
+                                title: Text(
+                                  details["name"],
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                                subtitle: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      details["age"] +
+                                          "\n" +
+                                          details["medical"],
+                                      style: TextStyle(fontSize: 15),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                });
+          },
+        )
       ])),
     );
   }
