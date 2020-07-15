b29d3b3c57bb469cb2c7b4342ef95a10feaf1e84
sayannath <sayannath235@gmail.com>
2020-07-15 21:55:38 +0530

    Nevereding Project

--- lib/pages/SOS/routes/disasterOne.dart
+++ lib/pages/SOS/routes/disasterOne.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rahat/custom_widgets/google_add.dart';
 
 class DisasterOne extends StatefulWidget {
   DisasterOne({this.uid});
@@ -17,51 +18,87 @@ class _DisasterOneState extends State<DisasterOne> {
       width: 100.0,
       alignment: FractionalOffset.center);
 
+  // ignore: avoid_init_to_null
+  int _selectedIndex = null;
+  bool selected = false;
+
+  _onSelected(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  List<DocumentSnapshot> people = <DocumentSnapshot>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 1.0,
-          title: appLogo,
-          centerTitle: true,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: widget.uid == null
-              ? Center(child: Text('Loading...'))
-              : Container(
-                  child: StreamBuilder(
-                      stream: Firestore.instance
-                          .collection("users")
-                          .document(widget.uid)
-                          .collection("person")
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        return ListView.builder(
-                            itemCount: snapshot.data.documents.length,
-                            itemBuilder: (context, index) {
-                              DocumentSnapshot details =
-                                  snapshot.data.documents[index];
-                              return details == null
-                                  ? Center(child: Text("Loading..."))
-                                  : CheckboxListTile(
-                                      value: _isChecked,
-                                      onChanged: (val) {
-                                        setState(() {
-                                          _isChecked = val;
-                                        });
-                                      },
-                                      title: Text(
-                                        details["name"],
-                                        style: TextStyle(
-                                            color: Colors.black, fontSize: 20),
-                                      ),
-                                    );
-                            });
-                      }),
-                ),
-        ));
+        elevation: 1.0,
+        title: appLogo,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: widget.uid == null
+            ? Center(child: Text('Loading...'))
+            : Container(
+                child: StreamBuilder(
+                    stream: Firestore.instance
+                        .collection("users")
+                        .document(widget.uid)
+                        .collection("person")
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      return ListView.builder(
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot details =
+                                snapshot.data.documents[index];
+                            for (var i = 1;
+                                i < snapshot.data.documents.length;
+                                i++) people.add(snapshot.data.documents[i]);
+                            return details == null
+                                ? Center(child: Text("Loading..."))
+                                // : Container(
+                                //     color: _selectedIndex != null &&
+                                //             _selectedIndex == index
+                                //         ? Colors.red
+                                //         : Colors.white,
+                                //     child: ListTile(
+                                //       title: Text(
+                                //         details["name"],
+                                //         style: TextStyle(color: Colors.black),
+                                //       ),
+                                //       onTap: () => _onSelected(index),
+                                //     ),
+                                //   );
+                                : ListTile(
+                                    onTap: () {
+                                      setState(() {
+                                        people[index].selected =
+                                            !people[index].selected;
+                                      });
+                                    },
+                                    selected: people[index].selected,
+                                    title: Text(details["name"]),
+                                    trailing: (people[index].selected)
+                                        ? Icon(Icons.check_box)
+                                        : Icon(Icons.check_box_outline_blank),
+                                  );
+                          });
+                    }),
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {},
+      ),
+    );
   }
+}
+
+class People {
+  bool selected = false;
 }
